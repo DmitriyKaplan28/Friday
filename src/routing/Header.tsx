6195ecf20b391e1ebf872ab552485f7254745815
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-08-24 01:44:31 +0300

    feat: add style for Header

--- src/routing/Header.tsx
+++ src/routing/Header.tsx
@@ -4,25 +4,28 @@ import s from './Header.module.css'
 
 function Header() {
   return (
-    <div className={s.defaultHeader}>
-      <NavLink to={'/profile'} className={s.links}>
-        profile
-      </NavLink>
-      <NavLink to={'/login'} className={s.links}>
-        login
-      </NavLink>
-      <NavLink to={'/sign-up'} className={s.links}>
-        sign up
-      </NavLink>
-      <NavLink to={'/reset-password'} className={s.links}>
-        reset password
-      </NavLink>
-      <NavLink to={'/enter-new-password'} className={s.links}>
-        enter new password
-      </NavLink>
-      <NavLink to={'/test'} className={s.links}>
-        test
-      </NavLink>
+    <div className={s.header}>
+      <div className={s.incubator}></div>
+      <div className={s.button}>
+        <NavLink to={'/login'} className={s.links}>
+          Sing in
+        </NavLink>
+      </div>
+      {/*<NavLink to={'/profile'} className={s.links}>*/}
+      {/*  profile*/}
+      {/*</NavLink>*/}
+      {/*<NavLink to={'/sign-up'} className={s.links}>*/}
+      {/*  sign up*/}
+      {/*</NavLink>*/}
+      {/*<NavLink to={'/reset-password'} className={s.links}>*/}
+      {/*  reset password*/}
+      {/*</NavLink>*/}
+      {/*<NavLink to={'/enter-new-password'} className={s.links}>*/}
+      {/*  enter new password*/}
+      {/*</NavLink>*/}
+      {/*<NavLink to={'/test'} className={s.links}>*/}
+      {/*  test*/}
+      {/*</NavLink>*/}
     </div>
   )
 }
