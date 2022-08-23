3e138c9846488c719eb1767c2113ee945a5c1b3d
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-08-24 00:18:08 +0300

    feat: add ErrorSnackbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import PageRouting from './routing/PageRouting'
+import { ErrorSnackbar } from './common/c9-ErrorSnackbar/ErrorSnackbar'
 
 function App() {
   return (
     <div>
+      <ErrorSnackbar />
       <PageRouting />
     </div>
   )
---
+++ src/common/c9-ErrorSnackbar/ErrorSnackbar.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import Snackbar from '@mui/material/Snackbar'
+import MuiAlert, { AlertProps } from '@mui/material/Alert'
+import { setError, useAppDispatch, useAppSelector } from '../../reducers/signup-reducer'
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+})
+
+export const ErrorSnackbar = () => {
+  const dispatch = useAppDispatch()
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    dispatch(setError(null))
+  }
+  const error = useAppSelector((state) => state.register.error)
+  const isOpen = error !== null
+
+  return (
+    <Snackbar open={isOpen} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+        This is a {error} message!
+      </Alert>
+    </Snackbar>
+  )
+}
