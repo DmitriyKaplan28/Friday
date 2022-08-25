31a5f1ea0e5a5ee2f74b741f8bfd684fc51af899
EnzoK <kropotin_aleksey@mail.ru>
2022-08-25 09:32:47 +0300

    fix: Card module css

--- src/routing/Header.tsx
+++ src/routing/Header.tsx
@@ -5,10 +5,6 @@ import logo from '../assets/logo.svg'
 import { Button } from '@mui/material'
 function Header() {
   return (
-    // <div className={s.header}>
-    //   <div className={s.incubator}>
-    //
-    //   </div>
     <header className={s.header}>
       <div className={s.container}>
         <div className={s.wrapper}>
@@ -18,27 +14,6 @@ function Header() {
             </NavLink>
           </div>
           <Button variant="contained">Sing in</Button>
-          {/*<div className={s.button}>*/}
-          {/*  <NavLink to={'/login'} className={s.links}>*/}
-          {/*    Sing in*/}
-          {/*  </NavLink>*/}
-          {/*</div>*/}
-          {/*<NavLink to={'/profile'} className={s.links}>*/}
-          {/*  profile*/}
-          {/*</NavLink>*/}
-          {/*<NavLink to={'/sign-up'} className={s.links}>*/}
-          {/*  sign up*/}
-          {/*</NavLink>*/}
-          {/*<NavLink to={'/reset-password'} className={s.links}>*/}
-          {/*  reset password*/}
-          {/*</NavLink>*/}
-          {/*<NavLink to={'/enter-new-password'} className={s.links}>*/}
-          {/*  enter new password*/}
-          {/*</NavLink>*/}
-          {/*<NavLink to={'/test'} className={s.links}>*/}
-          {/*  test*/}
-          {/*</NavLink>*/}
-          {/*</div>*/}
         </div>
       </div>
     </header>
