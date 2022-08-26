612cbd27592b4bb110bf34aad011937e1bd22af4
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-08-26 12:57:57 +0300

    feat: eyeClose

--- src/common/c10-ShowPassword/ShowPassword.tsx
+++ src/common/c10-ShowPassword/ShowPassword.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 import eye from '../../assets/register/eye.png'
+import eyeClose from '../../assets/register/eyeClose.png'
 
 import s from './ShowPassword.module.css'
 
@@ -7,13 +10,15 @@ type ShowPasswordType = {
   callback: (value: string) => void
 }
 export const ShowPassword = (props: ShowPasswordType) => {
+  const [link, setLink] = useState<boolean>(false)
   const onChangeHandler = () => {
     props.value === 'password' ? props.callback('text') : props.callback('password')
+    setLink(!link)
   }
 
   return (
-    <div className={s.showPassword}>
-      <img src={eye} alt="" onClick={onChangeHandler} />
+    <div className={s.showPassword} onClick={onChangeHandler}>
+      {link ? <img src={eye} alt="" /> : <img src={eyeClose} alt="" />}
     </div>
   )
 }
