113612de5e8be83875977a39199d0311f27c6d2b
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-09-07 21:16:10 +0300

    ref: delete style

--- src/components/Modal/CardsModal/CustomCardsModal.tsx
+++ src/components/Modal/CardsModal/CustomCardsModal.tsx
@@ -27,18 +27,6 @@ export type ModalCardsType = {
   setModeModal: (value: ModeModalType) => void
 }
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-}
-
 export const CustomCardsModal: React.FC<ModalCardsType> = ({
   children,
   callback,
